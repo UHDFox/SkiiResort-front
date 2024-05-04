0b70394978849686ab428daa1b1c809152dc84dc
UHDFox <uhdfox@yandex.ru>
2024-05-04 20:30:31 +0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -7,91 +7,37 @@ function WelcomePageRedirection()
 
 function VisitorRegister()
 {
-    alert("capybara pochesan");
+    var form = document.getElementById('visitorForm');
 
+    var formData = new FormData(form);
 
-    let name = document.getElementById("inputFirstName").value;
-    let age = document.getElementById("ageInput").value;
-    let phone = document.getElementById("phoneInput").value;
-    let birthday = document.getElementById("birthdayInput").value;
-    let passport = document.getElementById("passportInput").value;
-
-    let userData = document.getElementById("inputData").value;
-
-    let formData = new FormData(userData);
-
-    let testForm = new FormData();
-    testForm.append("name", name);
-    testForm.append("age", age);
-    testForm.append("phone", phone);
-    testForm.append("birthday", birthday);
-    testForm.append("passport", passport);  
-
+    var jsonData = {};
     
+    formData.forEach((value, key) => {
+      jsonData[key] = value;
+    });
 
-    let xhr = new XMLHttpRequest()
-    xhr.open("POST", "https://localhost:7046/api/Visitor/")
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.send(testForm);
-
-    xhr.onload = function() {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            var response = JSON.parse(xhr.responseText);
-            console.log('New item added:', response);
-        } else {
-            console.error('Error adding new item:', xhr.statusText);
-        }
-    };
-
-   
+    fetch('https://localhost:7046/api/Visitor', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(jsonData)
+    })    
+  }
 
 
-
-   
-}
-
-
-/** 
-
-function WelcomePageRedirection()
-{
-    window.location.href = "pageTwo.html";
-}
-
-function VisitorRegister()
-{
-    alert("capybara pochesan");
-
-    let name = document.getElementById("inputFirstName").value.concat(' ', document.getElementById("inputSecondName").value);
-    let email = document.getElementById("emailInput").value;
-    let age = document.getElementById("ageInput").value;
-    let phone = document.getElementById("phoneInput").value;
-    let birthday = document.getElementById("birthdayInput").value;
-    let passport = document.getElementById("passportInput").value;
-
-    let request = {
-        'name': name,
-        'age' : age,
-        'phone' : phone,
-        'birthdate' : birthday
+  function GetVisitorsList()
+  {
+    var x = document.getElementById("visitorsList");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } 
+    else 
+    {
+      x.style.display = "none";
     }
 
+  }
 
-    let xhr = new XMLHttpRequest()
-    xhr.open("POST", "https://localhost:7046/api/Visitor/Add", true)
-    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhr.send(JSON.stringify(request));
-    
-    xhr.onload = function() {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            var response = JSON.parse(xhr.responseText);
-            console.log('New item added:', response);
-        } 
-    };
-
-    xhr.onerror = function() {
-        console.error('Error adding new item');
-    };
-}
-*/
+  
